5f1422f18db875724dc0d4300f5e5c8626eb1c06
Alejandro Giandinoto <momowalter@hotmail.com>
2022-03-04 16:02:54 +0000

    Primer add

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import { Header } from './components/Header.js'
+import { Main } from './components/Main'
+import { Footer } from './components/Footer'
+
+function App( ) {
+
+  return (
+    <div className="App">
+      <>
+       <Header />
+       <Main  />
+       <Footer />
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Navbar} from './Navbar.js'
+
+export const Header = () => {
+  return (
+    <header className="App-header">
+        <h1>My eCommerce</h1>
+        <>
+           <Navbar />
+        </>
+    </header>
+  )
+}
+
+
+export default Header 
